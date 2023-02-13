d6a25aaca387c76d5e0632b67b05bd8e6771723c
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-02-13 14:55:58 +0900

    Ajax 인증 방식 전용 필터&토큰 생성

--- src/main/java/io/security/corespringsecurity/security/filter/AjaxLoginProcessingFilter.java
+++ src/main/java/io/security/corespringsecurity/security/filter/AjaxLoginProcessingFilter.java
@@ -2,6 +2,7 @@ package io.security.corespringsecurity.security.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.security.corespringsecurity.domain.AccountDto;
+import io.security.corespringsecurity.security.token.AjaxAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -17,29 +18,31 @@ public class AjaxLoginProcessingFilter extends AbstractAuthenticationProcessingF
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    protected AjaxLoginProcessingFilter() {
+    public AjaxLoginProcessingFilter() {
         super(new AntPathRequestMatcher("/api/login"));
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
 
-        if (!isAjax(request)) {
-            throw new IllegalStateException("Authentication is not supported");
+        if ( !isAjax( request ) ) {
+            throw new IllegalStateException( "Authentication is not supported" );
         }
 
         AccountDto accountDto = objectMapper.readValue(request.getReader(), AccountDto.class);
 
-        if (!StringUtils.hasText(accountDto.getUsername()) || !StringUtils.hasText(accountDto.getPassword())) {
-            throw new IllegalArgumentException("Username or Password is empty");
+        if ( accountDto.getUsername().isEmpty() || accountDto.getPassword().isEmpty() ) {
+            throw new IllegalArgumentException( "Username or Password is empty" );
         }
 
-        return null;
+        AjaxAuthenticationToken ajaxAuthenticationToken = new AjaxAuthenticationToken(accountDto.getUsername(), accountDto.getPassword());
+
+        return getAuthenticationManager().authenticate( ajaxAuthenticationToken );
     }
 
     private boolean isAjax(HttpServletRequest request) {
 
-        if ("XMLHTTPRequest".equals(request.getHeader("X-Requested-with"))) {
+        if ( "XMLHttpRequest".equals( request.getHeader( "X-Requested-With" ) ) ) {
             return true;
         }
         return false;
--- src/main/java/io/security/corespringsecurity/security/token/AjaxAuthenticationToken.java
+++ src/main/java/io/security/corespringsecurity/security/token/AjaxAuthenticationToken.java
@@ -1,5 +1,58 @@
 package io.security.corespringsecurity.security.token;
 
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.SpringSecurityCoreVersion;
+import org.springframework.util.Assert;
+
+import java.util.Collection;
+
 public class AjaxAuthenticationToken extends AbstractAuthenticationToken {
 
+    private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+    private final Object principal;
+    private Object credentials;
+
+    /**
+     * 인증을 받기 전 사용자가 입력하는 정보를 담는 생성자
+     * (username, password)
+     */
+    public AjaxAuthenticationToken(Object principal, Object credentials) {
+        super((Collection)null);
+        this.principal = principal;
+        this.credentials = credentials;
+        this.setAuthenticated(false);
+    }
+
+    /**
+     * 인증 이후 인증에 성공한 결과를 담는 생성자
+     * @param principal 인증에 성공한 user 객체
+     * @param credentials  패스워드
+     * @param authorities 권한 정보
+     */
+    public AjaxAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.principal = principal;
+        this.credentials = credentials;
+        super.setAuthenticated(true);
+    }
+
+    public Object getCredentials() {
+        return this.credentials;
+    }
+
+    public Object getPrincipal() {
+        return this.principal;
+    }
+
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        Assert.isTrue(!isAuthenticated, "Cannot set this token to trusted - use constructor which takes a GrantedAuthority list instead");
+        super.setAuthenticated(false);
+    }
+
+    public void eraseCredentials() {
+        super.eraseCredentials();
+        this.credentials = null;
+    }
 }
